534ffc92eeaf291b720eb5a1d246b03b81063659
jermz678 <jermz678@live.com>
2021-07-28 18:47:44 -0500

    initial commit into develop.  We will make branches off of here

---
+++ fightsite1.0/src/components/login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Login(){
+    return (
+        <h1>ITS WORKING</h1>
+    )
+}
+
+export default Login;
---
+++ fightsite1.0/src/components/results.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Results(){
+    return (
+        <h1>ITS WORKING</h1>
+    )
+}
+
+export default Results;
