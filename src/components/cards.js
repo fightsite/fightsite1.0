d7ccc426c0c8dace3d4d7698fcc19caef5f0188c
jasonmueller <jmuel9999@gmail.com>
2021-07-30 18:50:12 -0500

    Prototype cards all created

--- src/components/cards.js
+++ src/components/cards.js
@@ -37,114 +37,114 @@ function Cards () {
             </FighterCards>
 
             <FighterCards
-                fighterName=""
-                strength="Str: "
-                speed="Spd: "
-                weight="Wgt:  lbs"
+                fighterName="Jonah Hill"
+                strength="Str: 3"
+                speed="Spd: .01"
+                weight="Wgt: 652 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName=""
-                strength="Str: "
-                speed="Spd: "
-                weight="Wgt:  lbs"
+                fighterName="Carrie Underwood"
+                strength="Str: 12"
+                speed="Spd: 75"
+                weight="Wgt: 16 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName=""
-                strength="Str: "
-                speed="Spd: "
-                weight="Wgt:  lbs"
+                fighterName="Kitty Purry"
+                strength="Str: 0"
+                speed="Spd: 1"
+                weight="Wgt: 9 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Morgan Freeman"
-                strength="Str: 14"
-                speed="Spd: 9"
-                weight="Wgt: 191 lbs"
+                fighterName="Bruce Campbell"
+                strength="Str: 455"
+                speed="Spd: 60"
+                weight="Wgt: 200 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Timmy Turner"
-                strength="Str: 10"
-                speed="Spd: 18"
-                weight="Wgt: 31 lbs"
+                fighterName="Burt Reynolds"
+                strength="Str: 8000"
+                speed="Spd: 8000"
+                weight="Wgt: 25 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Morgan Freeman"
-                strength="Str: 14"
-                speed="Spd: 9"
-                weight="Wgt: 191 lbs"
+                fighterName="Bender"
+                strength="Str: 9500"
+                speed="Spd: 500"
+                weight="Wgt: 600 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Timmy Turner"
-                strength="Str: 10"
-                speed="Spd: 18"
-                weight="Wgt: 31 lbs"
+                fighterName="Johnny Bravo"
+                strength="Str: 250"
+                speed="Spd: 2"
+                weight="Wgt: 234 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Morgan Freeman"
-                strength="Str: 14"
-                speed="Spd: 9"
-                weight="Wgt: 191 lbs"
+                fighterName="Bob the Builder"
+                strength="Str: 80"
+                speed="Spd: 15"
+                weight="Wgt: 6 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Timmy Turner"
-                strength="Str: 10"
-                speed="Spd: 18"
-                weight="Wgt: 31 lbs"
+                fighterName="God"
+                strength="Str: 9999"
+                speed="Spd: ???"
+                weight="Wgt: ??? lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Morgan Freeman"
-                strength="Str: 14"
-                speed="Spd: 9"
-                weight="Wgt: 191 lbs"
+                fighterName="Master Chief"
+                strength="Str: 250"
+                speed="Spd: 50"
+                weight="Wgt: 205 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Timmy Turner"
-                strength="Str: 10"
-                speed="Spd: 18"
-                weight="Wgt: 31 lbs"
+                fighterName="Little Foot"
+                strength="Str: 5"
+                speed="Spd: 5"
+                weight="Wgt: 110 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Morgan Freeman"
-                strength="Str: 14"
-                speed="Spd: 9"
-                weight="Wgt: 191 lbs"
+                fighterName="Superman"
+                strength="Str: 9998"
+                speed="Spd: 9854642452585.64"
+                weight="Wgt: 220 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Timmy Turner"
-                strength="Str: 10"
-                speed="Spd: 18"
-                weight="Wgt: 31 lbs"
+                fighterName="Nathan Drake"
+                strength="Str: 100"
+                speed="Spd: 100"
+                weight="Wgt: 150 lbs"
                 >
             </FighterCards>
 
             <FighterCards
-                fighterName="Morgan Freeman"
-                strength="Str: 14"
-                speed="Spd: 9"
-                weight="Wgt: 191 lbs"
+                fighterName="Will Ferrell"
+                strength="Str: 56"
+                speed="Spd: 56"
+                weight="Wgt: 245 lbs"
                 >
             </FighterCards>
         </div>
