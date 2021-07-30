46acc3499d3f14c8ebce099e129d20991d0931cd
jermz678 <jermz678@live.com>
2021-07-30 15:24:10 -0500

    starting design on header and footer

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer(){
+    return (
+        <div className='footer-div'>
+            <h2>Created by Jason, Jeremiah, Marc, and Peter</h2>
+        </div>
+    )
+}
+export default Footer;
