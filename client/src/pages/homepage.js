18c506f10e4b31a35b4dafda4eed9ea87e8dd866
P Stenger <pstenger23@gmail.com>
2021-08-02 12:11:24 -0500

    a little useState/handle form submit

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ChooseFighter from '../components/choose-fighter';
 import ChooseBets from '../components/choose-bet';
 import Fight from '../components/fight';
-import Login from '../components/login';
 import Results from '../components/results';
 import FighterCards from '../components/fightercards';
 import Cards from '../components/cards';
@@ -10,12 +9,45 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import Animation from '../components/animation';
 import SignIn from '../components/sign-in';
+import { useState } from 'react'
 
 function HomePage() {
+    const admin = {
+        email: "pete@pete.com",
+        password: 'peter'
+    }
+
+    const [user, setUser] = useState({email: ""});
+    const [error, setError] = useState("");
+
+    const login = userInfo => {
+        console.log(userInfo);
+        if(userInfo.email === admin.email && userInfo.password === admin.password) {
+            console.log('you made it');
+        }
+        else {
+            console.log('idiot');
+        }
+    }
+
+    const logout = () => {
+        console.log("logout");
+    }
+
+    const createUser = (details) => {
+        console.log(details);
+
+    }
 return (
     <div className='flex-project'>
         <Header></Header>
-        <SignIn></SignIn>
+        {(user.email = "") ? (
+           <Animation></Animation>
+        ) : (
+            <SignIn LogIn={login} signUp={createUser} error={error} ></SignIn>
+            
+        )}
+        
         {/* <ChooseBets></ChooseBets> */}
         {/* <ChooseFighter></ChooseFighter>
         <Fight></Fight>
