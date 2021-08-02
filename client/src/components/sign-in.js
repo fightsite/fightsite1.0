904771b76bdc3b56fb8c0c2a97ced5c125691d5f
jasonmueller <jmuel9999@gmail.com>
2021-08-01 22:42:03 -0500

    Sign in and Sign up basic forms complete. Need a LOT of work for media queries.

--- client/src/components/sign-in.js
+++ client/src/components/sign-in.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 function signIn () {
     return (
-        <section className="signIn-form-main-container">
+    <main>  
+        <section className="signIn-form-main-container1">
             <div className="form-title">
-                <h2>Sign up to start the fights!</h2>
+                <h2>Sign in to start the fights!</h2>
             </div>
             <div className="form-info">
                 <form className="form">
@@ -14,11 +15,34 @@ function signIn () {
                     <label for="password"><b className="pass-title">Password:</b></label>
                     <input type="password" placeholder="Enter password" name="password" required/>
                     <div className="btn-div">
-                        <button type="submit">Create Account</button>
+                        <button className="signIn-btns" type="submit">Login</button>
                     </div>
                 </form>
             </div>
         </section>
+        <section className="divider">
+            <div>
+                <h2>Don't have an account? No Problem!</h2>
+            </div>
+        </section>
+        <section className="signIn-form-main-container2">
+            <div className="form-title">
+                <h2>Sign up with us now!</h2>
+            </div>
+            <div className="form-info">
+                <form className="form">
+                    <label for="email"><b>Email:</b></label>
+                    <input type="text" placeholder="Enter email" name="email" required/>
+
+                    <label for="password"><b className="pass-title">Password:</b></label>
+                    <input type="password" placeholder="Enter password" name="password" required/>
+                    <div className="btn-div">
+                        <button className="signIn-btns" type="submit">Create Account</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </main>      
     )
 };
 
