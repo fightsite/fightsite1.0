5513b542fc6299400221aa590efb72ad5197ef92
jermz678 <jermz678@live.com>
2021-08-02 22:51:23 -0500

    created results page with random logic to choose who wins

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -1,30 +1,47 @@
 import React from 'react';
-import Coin from '../assets/coin.png'
-function Results(){
-    return (
-        <div className="coin-holder">
-        <img  className="coin1" src={Coin}></img>
-        <img  className="coin2" src={Coin}></img>
-        <img  className="coin3" src={Coin}></img>
-        <img  className="coin4" src={Coin}></img>
-        <img  className="coin5" src={Coin}></img>
-        <img  className="coin6" src={Coin}></img>
-        <img  className="coin7" src={Coin}></img>
-        <img  className="coin8" src={Coin}></img>
-        <img  className="coin9" src={Coin}></img>
-        <img  className="coin10" src={Coin}></img>
-        <img  className="coin11" src={Coin}></img>
-        <img  className="coin12" src={Coin}></img>
-        <img  className="coin13" src={Coin}></img>
-        <img  className="coin14" src={Coin}></img>
-        <img  className="coin15" src={Coin}></img>
-        <img  className="coin16" src={Coin}></img>
-        <img  className="coin17" src={Coin}></img>
-        <img  className="coin18" src={Coin}></img>
-        <img  className="coin19" src={Coin}></img>
-        <img  className="coin20" src={Coin}></img>
 
-        </div>
+function Results(){
+
+//an array to hold one liners
+    const quips = [
+        "Holy Moly, that was a close one!",
+        "Good Effort!",
+        "This is what you deserve...",
+        "I can't believe it!",
+        "Sometimes its just dumb luck",
+        "Who saw this coming??",
+        "Were you even f*#@ing trying?",
+        "Thanks for shopping at *#@-whooping R-US",
+        "You seem like more of an indoor kitty cat"
+        ]
+    //using math.random to go through one liners  
+    let randomQuip = Math.floor(Math.random() * quips.length);
+    let quipChosen  = quips[randomQuip];
+        
+    //an array to hold results
+    const results = [
+        'Winner',
+        'Loser'
+    ]
+    //using math.random to go through results
+    let randomResult = Math.floor(Math.random() * results.length);
+    let resultChosen = results[randomResult]
+
+    return (
+        <div className='results'>
+            <div className='quip'> 
+                <h2> {quipChosen} </h2>
+            </div>
+            <div className="win-or-lose">
+                <h2>{resultChosen}!!!!</h2>
+            </div>
+            <div className="money">
+                <h2>You just won $500! </h2>
+            </div>
+            <div className="results-btn-holder">
+                <button className="results-btn">Play Again</button>
+            </div>
+         </div>
     )
 }
 
