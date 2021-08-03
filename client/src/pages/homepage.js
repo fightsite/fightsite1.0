dbf6a17f8c33ce2335ea9ff9355758479788d783
jermz678 <jermz678@live.com>
2021-08-03 15:01:10 -0500

    created a function to get a random opponent

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import ChooseFighter from '../components/choose-fighter';
-import ChooseBets from '../components/choose-bet';
 import Fight from '../components/fight';
 import Results from '../components/results';
-import FighterCards from '../components/fightercards';
-import Cards from '../components/cards';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import Animation from '../components/animation';
 import SignIn from '../components/sign-in';
 import { useState } from 'react'
 
-function HomePage({}) {
+function HomePage() {
     const admin = {
         email: "pete@pete.com",
         password: 'peter'
     }
 
     const [user, setUser] = useState({email: ""});
-    const [currentFighter, setCurrentFighter] = useState({name: ""});
     const [error, setError] = useState("");
 
     const login = userInfo => {
-        
+        console.log(userInfo);
         if(userInfo.email === admin.email && userInfo.password === admin.password) {
             setUser({ email: userInfo.email });
             console.log('you made it');
@@ -31,10 +27,7 @@ function HomePage({}) {
             console.log('idiot');
         }
     }
-    const getFighter = fighterInfo => {
-        console.log(fighterInfo);
-        setCurrentFighter({name: fighterInfo});
-    }
+
     const logout = () => {
         console.log("logout");
     }
@@ -46,27 +39,24 @@ function HomePage({}) {
 return (
     <div className='flex-project'>
         <Header></Header>
-       {(user.email != "") ? (
-           <ChooseFighter getFighter={getFighter}></ChooseFighter>
-        ) : (user.email != "" && currentFighter != "") ? (
-            <Fight></Fight>
-            ) : (
-                <SignIn LogIn={login} signUp={createUser} error={error} ></SignIn>
+        <ChooseFighter></ChooseFighter>
+        {/*(user.email != "") ? (
+           <Animation></Animation>
         
+        ) : (
+            <SignIn LogIn={login} signUp={createUser} error={error} ></SignIn>
+            
         )}
-        {/* {(user.email && currentFighter.name)} */}
         
-
         {/* <ChooseBets></ChooseBets> */}
-        
-        {/* <FighterCards></FighterCards> */}
-        {/* <Fight></Fight> */}
-        {/* <Login></Login> */}
-        {/* <Results></Results> */}
+        {/* <ChooseFighter></ChooseFighter>
+        <Fight></Fight>
+        <Login></Login>
+        <Results></Results> */}
         <Footer></Footer>
   </div>
 )
-   
+    
 }
 
 export default HomePage;
